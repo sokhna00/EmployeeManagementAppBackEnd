24fc6ecab96f8888858ed76711206b7211ca8f8d
sokhna00 <dsokhnadiarra@ept.sn>
2021-08-08 14:11:17 +0000

    second commit

--- src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
+++ src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -1,10 +1,10 @@
 package tech.getarrays.employeemanager.service;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.getarrays.employeemanager.exception.UserNotFoundException;
 import tech.getarrays.employeemanager.model.Employee;
 import tech.getarrays.employeemanager.repository.EmployeeRepository;
-
+import java.util.List;
 import java.util.UUID;
 
 
@@ -21,5 +21,20 @@ public class EmployeeService {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
 
+    }
+
+    public List<Employee> findAllEmployess(){
+        return employeeRepository.findAll();
+    }
+    public  Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id" + id + "was not found"));
+    }
+    public void deleteEmployee(Long id){
+           employeeRepository.deleteEmployeeById(id);
     }
 }
